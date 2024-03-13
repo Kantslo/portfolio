d53ab382373f8daea4e38fec5ceda9cb1a6fa141
Kantslo <gqancliani@gmail.com>
2024-03-13 05:56:24 +0400

    added about page to describe myself

---
+++ components/about.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function About() {
+  return <section>
+    <h2>About me</h2>
+    <p className="mb-3">
+        After enrolling in university,{" "}
+        I decided to pursue my passion for programming. I enrolled in a coding bootcamp and learned{" "}
+        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">
+          React, Next.js, Express.js, and MongoDB
+        </span>
+        . I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching movies, and playing with my dog.
+      </p>
+  </section>
+}
