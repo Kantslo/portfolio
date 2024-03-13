508042e98f722e69461169611889fc1ed0d3a953
Kantslo <gqancliani@gmail.com>
2024-03-13 05:03:06 +0400

    animated the intro text with framer motion

--- components/intro.tsx
+++ components/intro.tsx
@@ -38,7 +38,7 @@ export default function Intro() {
       </div>
     </div>
 
-    <motion.p className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+    <motion.h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
     initial={{ opacity: 0, y: 100 }}
     animate={{ opacity: 1, y: 0 }}
     >
@@ -46,7 +46,7 @@ export default function Intro() {
         <span className="font-bold">full-stack developer.</span> I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
-    </motion.p>
+    </motion.h1>
 
       <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
       initial={{ opacity: 0, y: 100 }}
