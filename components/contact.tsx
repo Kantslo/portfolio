689e3c0eb8dddb5cb98b49392633b5adb9872548
Kantslo <gqancliani@gmail.com>
2024-03-14 22:53:48 +0400

    improved error handling for contact form

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,11 +1,11 @@
 "use client"
 
 import { motion } from "framer-motion"
-import { FaPaperPlane } from "react-icons/fa";
 
+import SubmitBtn from "./submit-btn";
+import SectionHeading from "./section-heading";
 import { sendEmail } from "@/actions/sendEmail";
 import { useSectionInView } from "@/lib/hooks";
-import SectionHeading from "./section-heading";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact")
@@ -21,12 +21,19 @@ export default function Contact() {
     <p className="text-gray-700 -mt-6">Please contact me directly at <a className="underline" href="mailto:gqancliani@gmail.com">gqancliani@gmail.com</a> or through this form.</p>
     <form 
     action={async (formData) => {
-      await sendEmail(formData)
+      const {data, error} = await sendEmail(formData)
+
+      if (error) {
+        alert(error);
+        return;
+      }
+
+      alert("Email sent successfully!")
     }} 
     className="mt-10 flex flex-col">
       <input className="h-14 rounded-lg borderBlack px-4" type="email" required maxLength={500} placeholder="Your email" name="senderEmail" />
       <textarea className="h-52 my-3 rounded-lg borderBlack p-4" placeholder="Your message" required maxLength={5000} name="message" />
-      <button type="submit" className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105">Submit <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" /> </button>
+      <SubmitBtn />
     </form>
   </motion.section>
 }
