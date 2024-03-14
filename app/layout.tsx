cdd123f3f78f1deca445e077abcfbd60a99241a9
Kantslo <gqancliani@gmail.com>
2024-03-14 23:12:37 +0400

    added footer to the portfolio website

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,9 @@ import { Inter } from "next/font/google";
 
 import { Toaster } from "react-hot-toast";
 
-import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 import "./globals.css";
 
@@ -29,6 +30,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Footer />
           <Toaster position="top-right" />
         </ActiveSectionContextProvider>
         </body>
