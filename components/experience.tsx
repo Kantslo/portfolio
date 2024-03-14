1ca9fd6cb62fdc86bd2d5b97c4bd818aae3a302a
Kantslo <gqancliani@gmail.com>
2024-03-14 18:30:02 +0400

    added useSectionInView hook to experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -5,11 +5,14 @@ import React from "react";
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
+import { useSectionInView } from "@/lib/hooks";
 import { experiencesData } from "@/lib/data";
 import SectionHeading from "./section-heading";
 
 export default function Experience() {
-  return <section id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
+  const { ref } = useSectionInView("Experience")
+
+  return <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
     <SectionHeading>My experience</SectionHeading>
     <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
