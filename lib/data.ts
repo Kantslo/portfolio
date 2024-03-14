65f9e35bf4451dfd7a957d38080c3026ccd116fe
Kantslo <gqancliani@gmail.com>
2024-03-15 01:25:33 +0400

    removed dummy experiences data from data file

--- lib/data.ts
+++ lib/data.ts
@@ -1,8 +1,6 @@
 import React from "react";
 
-import { LuGraduationCap } from "react-icons/lu";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
+import { FaSmile, FaCode } from "react-icons/fa";
 
 import memoryGameImg from "@/public/memory-game.png"
 import parkingAppImg from "@/public/parking-app-api.png"
@@ -36,28 +34,20 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "No job experience :/",
+    location: "",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
+      "You are going to have to help me with that!",
+    icon: React.createElement(FaSmile),
+    date: "present",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "This could have been you",
+    location: "",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+      "I could be working in your company right now, and this amazing timeline animation could be filled with more experiences.",
+    icon: React.createElement(FaCode),
+    date: "present",
   },
 ] as const;
 
