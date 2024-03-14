9593208bcf9de66624fd520d9ab7f42bdedf67b7
Kantslo <gqancliani@gmail.com>
2024-03-14 19:06:44 +0400

    added form validation to contact section

--- components/contact.tsx
+++ components/contact.tsx
@@ -3,6 +3,7 @@
 import { motion } from "framer-motion"
 import { FaPaperPlane } from "react-icons/fa";
 
+import { sendEmail } from "@/actions/sendEmail";
 import { useSectionInView } from "@/lib/hooks";
 import SectionHeading from "./section-heading";
 
@@ -18,9 +19,13 @@ export default function Contact() {
     <SectionHeading>Contact me</SectionHeading>
 
     <p className="text-gray-700 -mt-6">Please contact me directly at <a className="underline" href="mailto:gqancliani@gmail.com">gqancliani@gmail.com</a> or through this form.</p>
-    <form className="mt-10 flex flex-col">
-      <input className="h-14 rounded-lg borderBlack px-4" type="email" placeholder="Your email" />
-      <textarea className="h-52 my-3 rounded-lg borderBlack p-4" placeholder="Your message" />
+    <form 
+    action={async (formData) => {
+      await sendEmail(formData)
+    }} 
+    className="mt-10 flex flex-col">
+      <input className="h-14 rounded-lg borderBlack px-4" type="email" required maxLength={500} placeholder="Your email" name="senderEmail" />
+      <textarea className="h-52 my-3 rounded-lg borderBlack p-4" placeholder="Your message" required maxLength={500} name="message" />
       <button type="submit" className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105">Submit <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" /> </button>
     </form>
   </motion.section>
