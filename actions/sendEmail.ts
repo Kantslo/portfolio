7f0d21f7ca45f52ef59413e3c0d8619803e41c81
Kantslo <gqancliani@gmail.com>
2024-03-14 22:53:12 +0400

    installed resend package and made the email sending system

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,8 +1,11 @@
 "use server"
 
+import React from "react"
+
 import { Resend } from "resend"
 
 import { validateString, getErrorMessage } from "@/lib/utils"
+import ContactFormEmail from "@/email/contact-form-email"
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -22,17 +25,25 @@ export const sendEmail = async (formData: FormData) => {
     }
   }
 
+  let data;
   try {
-    await resend.emails.send({
+    data = await resend.emails.send({
       from: "Portfolio Contact Form <onboarding@resend.dev>",
       to: "gqancliani@gmail.com",
       subject: "Message from portfolio contact form",
       reply_to: senderEmail as string,
-      text: message as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
     }) 
   } catch (error: unknown) {  
     return {
       error: getErrorMessage(error),
     }
+  }
+
+  return {
+    data,
   }
 }
