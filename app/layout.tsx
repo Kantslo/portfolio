8d3cdde4558e864916e269dc26285bec6fbba1b3
Kantslo <gqancliani@gmail.com>
2024-03-14 23:00:41 +0400

    added toaster component from react-hot-toast in the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { Toaster } from "react-hot-toast";
+
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 
@@ -27,6 +29,7 @@ export default function RootLayout({
         <ActiveSectionContextProvider>
           <Header />
           {children}
+          <Toaster position="top-right" />
         </ActiveSectionContextProvider>
         </body>
     </html>
