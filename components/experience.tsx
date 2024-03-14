89f567771c67d7bd0e16d759832262e4cfa15b0d
Kantslo <gqancliani@gmail.com>
2024-03-14 17:59:52 +0400

    added scroll styling to experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -9,7 +9,7 @@ import { experiencesData } from "@/lib/data";
 import SectionHeading from "./section-heading";
 
 export default function Experience() {
-  return <section id="experience">
+  return <section id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
     <SectionHeading>My experience</SectionHeading>
     <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
