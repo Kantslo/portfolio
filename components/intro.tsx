3e78e945423551a52334b5318dc6cf36ed950c07
Kantslo <gqancliani@gmail.com>
2024-03-15 01:25:18 +0400

    updated my intro text

--- components/intro.tsx
+++ components/intro.tsx
@@ -50,7 +50,7 @@ export default function Intro() {
         <span className="font-bold">Hello, I'm Giorgi.</span> I'm a{" "}
         <span className="font-bold">full-stack developer.</span> I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">React (Next.js) & Node.js</span>.
     </motion.h1>
 
       <motion.div className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
