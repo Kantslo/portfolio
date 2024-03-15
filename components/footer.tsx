89b2ec3e9d0288fc6a22190a7491dcaff57a39f8
Kantslo <gqancliani@gmail.com>
2024-03-15 23:26:30 +0400

    chore: edited footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return <footer className='mb-10 px-4 text-center text-gray-500'>
     <small className='mb-2 text-xs block'>
-      &copy; 2030 Giorgi Kantsliani. All rights reserved. 
+      &copy; 2024 Giorgi Kantsliani. All rights reserved. 
     </small>
     <p className='text-xs'>
       <span className='font-semibold'>About this website:</span> built with React & Next.js (App router & Server Actions), Typescript, Tailwind CSS, Framer Motion, React Email & Resend, hosted on Vercel. 
