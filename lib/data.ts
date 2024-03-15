472fb0f3ba5ca796066b7a1d767f9870ca88e582
Kantslo <gqancliani@gmail.com>
2024-03-15 04:43:07 +0400

    updated my project data

--- lib/data.ts
+++ lib/data.ts
@@ -4,6 +4,8 @@ import { FaSmile, FaCode } from "react-icons/fa";
 
 import memoryGameImg from "@/public/memory-game.png"
 import parkingAppImg from "@/public/parking-app-api.png"
+import restCountriesImg from "@/public/rest-countries-api.png"
+import dictionaryAppImg from "@/public/dictionary-web-app.png"
 
 export const links = [
   {
@@ -54,16 +56,30 @@ export const experiencesData = [
 export const projectsData = [
   {
     title: "Parking App Api",
-    description: "Parking App Api let's you to register on parking application and add your vehicle, make parking reservations and connect your vehicles with parking lots",
+    description: "Seamlessly integrate our API into your application to access real-time parking availability, pricing information, and reservation capabilities. Whether you're a driver searching for the nearest parking spot or a business looking to optimize parking management, our API offers comprehensive features to meet your needs.",
     tags: ["Node.Js", "Express.Js", "Typescript", "Swagger"],
     imageUrl: parkingAppImg,
   },
   {
     title: "Memory Game",
     description:
-      "This is my first game I made with javascript, players can either choose number or icon mode for the game, and 4x4 and 6x6 board, players can start single-player mode, or play up to 4 players.",
+      "Enjoy my first javascript game I built for you! Choose between numbers and icons, and select from 4x4 or 6x6 board sizes for varying challenges. Exercise your brain, test your memory, and enjoy hours of entertainment!",
     tags: ["React", "Typescript", "Tailwind"],
     imageUrl: memoryGameImg,
+  },
+  {
+    title: "REST Countries API",
+    description:
+      "Welcome to our REST Countries API service, your gateway to a world of global information at your fingertips. With seamless integration and robust functionality, our API provides comprehensive data on countries worldwide. Explore detailed information including country names, population, languages spoken, currencies used, and much more.",
+    tags: ["React", "Tailwind", "Axios"],
+    imageUrl: restCountriesImg,
+  },
+  {
+    title: "Dictionary Web App",
+    description:
+      "Enhance your vocabulary with this dictionary app I built from frontend-mentor, access definitions, synonyms, and examples, empowering you to expand your linguistic horizons effortlessly.",
+    tags: ["React", "Tailwind", "Axios"],
+    imageUrl: dictionaryAppImg,
   },
 ] as const;
 
